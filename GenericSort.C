32d10e26c340eb3d5089524981431d39c5a243c1
JheneJin <80924562+JheneJin@users.noreply.github.com>
2023-04-05 07:04:40 -0700

    Create GenericSort.C

---
+++ GenericSort.C
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 20
+
+typedef struct
+{
+    char *key;
+    int value;
+} dict;
+
+// sorting functions
+void sort(void *a, size_t structSize, size_t numSize, int (*compare)(void *x, void *y))
+{
+    size_t len = structSize / numSize;
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = 0; j < len - i - 1; j++)
+        {
+            if (compare(a + numSize * j, a + numSize * (j + 1)) > 0)
+            {
+                char temp[numSize];
+                memcpy(temp, a + numSize * j, numSize);
+                memcpy(a + numSize * j, a + numSize * (j + 1), numSize);
+                memcpy(a + numSize * (j + 1), temp, numSize);
+            }
+        }
+    }
+}
+
+int compare_float(void *x, void *y)
+{
+    float *v1 = (float *)x;
+    float *v2 = (float *)y;
+    if (*v1 > *v2)
+    {
+        return 1;
+    }
+    return -1;
+}
+
+int compare_int(void *x, void *y)
+{
+    int *v1 = (int *)x;
+    int *v2 = (int *)y;
+    if (*v1 > *v2)
+    {
+        return 1;
+    }
+    return -1;
+}
+
+int compare_key(dict *x, dict *y)
+{
+    char *l1 = (dict *)x->key;
+    char *l2 = (dict *)y->key;
+    return strcmp(l1,l2);
+}
+
+int compare_value(dict *x, dict *y)
+{
+    int v1 = (dict *)x->value;
+    int v2 = (dict *)y->value;
+    char *l1 = (dict *)x->key;
+    char *l2 = (dict *)y->key;
+    if (v1 != v2)
+    {
+        if(v1 < v2){
+            return 1;
+        }
+        return -1;
+    }else{
+        if(l1 > l2){
+            return 1;
+        }
+        return -1;
+    }
+}
+
+int main()
+{
+    float numbers[] = {645.41, 37.59, 76.41, 5.31, -34.23, 1.11, 1.10, 23.46, 635.47, -876.32, 467.83, 62.25};
+    int numbers2[] = {90, 20, 50, 100, 10, -8, 0, -9, 60};
+    dict people[] =
+        {
+            {"Hal", 20},
+            {"Susann", 31},
+            {"Dwight", 19},
+            {"Kassandra", 21},
+            {"Lawrence", 25},
+            {"Cindy", 22},
+            {"Cory", 27},
+            {"Mac", 19},
+            {"Romana", 27},
+            {"Doretha", 32},
+            {"Danna", 20},
+            {"Zara", 23},
+            {"Rosalyn", 26},
+            {"Risa", 24},
+            {"Benny", 28},
+            {"Juan", 33},
+            {"Natalie", 25}};
+
+    // sort by numbers
+    sort(numbers, sizeof(numbers), sizeof(float), compare_float);
+    for (int i = 0; i < (sizeof(numbers) / sizeof(float)); i++)
+    {
+        printf("%0.2f ", numbers[i]);
+    }
+    printf("\n\n");
+
+    sort(numbers2, sizeof(numbers), sizeof(int), compare_int);
+    for (int i = 0; i < (sizeof(numbers2) / sizeof(int)); i++)
+    {
+        printf("%d ", numbers2[i]);
+    }
+    printf("\n\n");
+
+    printf("Before Sort\n");
+    for (int i = 0; i < (sizeof(people) / sizeof(dict)); i++)
+    {
+        printf("{%s : %d},\n", people[i].key, people[i].value);
+    }
+    printf("\n");
+  
+    // sort by keys/names
+    printf("Sort By Names\n");
+    sort(people, sizeof(people), sizeof(dict), compare_key);
+    for (int i = 0; i < (sizeof(people) / sizeof(dict)); i++)
+    {
+        printf("{%s : %d},\n", people[i].key, people[i].value);
+    }
+    printf("\n");
+
+    // sort by values/ages
+    printf("Sort By Ages\n");
+    sort(people, sizeof(people), sizeof(dict), compare_value);
+    for (int i = 0; i < (sizeof(people) / sizeof(dict)); i++)
+    {
+        printf("{%s : %d},\n", people[i].key, people[i].value);
+    }
+    printf("\n");
+    return 0;
+}
